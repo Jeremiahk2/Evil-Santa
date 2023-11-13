bb02f7dcb5e966455f59277ba5593c4ecde42e74
Adam <adamsuperswag69noscope@gmail.com>
2023-11-12 22:34:32 -0500

    started work on chests spawning weapons

--- Evil Santa/scenes/levels/Chest.gd
+++ Evil Santa/scenes/levels/Chest.gd
@@ -1,4 +1,8 @@
 extends CollisionShape2D
 
+signal chestOpened
+
 func _on_player_open_chest():
+	print("---------------------------")
+	chestOpened.emit()
 	queue_free()
--- Evil Santa/scenes/levels/inside.gd
+++ Evil Santa/scenes/levels/inside.gd
@@ -2,6 +2,9 @@ extends LevelParent
 
 var chest_opened = false
 
+@onready var shotgun = $shotgunPickup
+
+
 func _on_player_enter_gate():
 	#print("pressed e")
 	get_tree().change_scene_to_file("res://scenes/levels/final_boss.tscn")
@@ -14,3 +17,12 @@ func _on_player_open_chest():
 		print("opened")
 		chest_opened = true
 
+
+
+func _on_chest_chest_opened():
+	print("made it")
+	shotgun.visible = true
+
+
+func _on_ready():
+	shotgun.visible = false
